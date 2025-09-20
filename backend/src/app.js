ce764887e9797f7b2584dbae0714ce03169d4cd2
Jesús David Limón Hernández <jesus_limonhdz@hotmail.com>
2025-09-20 13:41:31 -0600

    Chore: estructura inicial del proyecto con frontend y backend configurados

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+// backend/src/app.js
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+app.use('/api', routes);
+
+module.exports = app;
